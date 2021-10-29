d494a5e1b363b507e630a26bc565b1b3b242f4ac
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-10-29 22:12:34 +0300

    fixing

--- pt.c
+++ pt.c
@@ -1,40 +1,46 @@
 #include <stdlib.h>
 #include "os.h"
 
-int NUM_OF_LEVELS = 5;
-
-uint64_t* vpnParts = (uint64_t*) calloc(5, sizeof(uint64_t));
-void break_vpn_to_parts(uint64_t* vpnPartsAddr, uint64_t vpn);
+int NUM_OF_LEVELS = 5, shift, i;
+uint64_t vpnPart, pte, newPpn, newVa, requiredPpn;
+uint64_t* currentVa;
 
 void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn)
 {
-    uint64_t* currentVa = phys_to_virt(pt << 12);
-    break_vpn_to_parts(vpnParts, vpn);
+    currentVa = phys_to_virt(pt << 12);
+
 	if (ppn == NO_MAPPING) // invalidating the mapping, if exists
     {
-        for (int i = 0; i < NUM_OF_LEVELS; i++)
+        for (i = 0; i < NUM_OF_LEVELS; i++)
         {
-            uint64_t pte = currentVa[vpnParts[i]];
-            if ((pte & 1) == 0) // reached invalid pte, no need to invalidate
+            shift = 36 - (9 * i); 
+            vpnPart = (vpn >> shift) & 0x1FF; // masking to get relevant 9 bits
+            pte = currentVa[vpnPart];
+            if (pte == NULL || (pte & 1) == 0) // reached invalid or not existing pte, no need to invalidate
             {
                 return;
             }
             // if pte is valid, continue to the next level
-            currentVa = 
+            // pte = pte >> 12; TODO: understand if needed - nullifying the 12 least significant bits
+            currentVa = phys_to_virt(pte << 12);
         }
-        uint64_t lastVpnPart = pageTable[NUM_OF_LEVELS - 1][vpnParts[NUM_OF_LEVELS - 1]];
-        pageTable[NUM_OF_LEVELS - 1][vpnParts[NUM_OF_LEVELS - 1]] = lastVpnPart & (~1); // mapping exists and should be invalid
+        pte = pte & (~1); // mapping exists and should be invalid
     }
     else // creating a new mapping, if needed
     {
         for (int i = 0; i < NUM_OF_LEVELS; i++)
         {
-            uint64_t pte = pageTable[i][vpnParts[i]];
-            if ((pte & 1) == 0) // reached invalid pte, creating new mapping
+            shift = 36 - (9 * i); 
+            vpnPart = (vpn >> shift) & 0x1FF; // masking to get relevant 9 bits
+            pte = currentVa[vpnPart];
+            if (pte == NULL) // mapping doesn't exist, creating new mapping
             {
-                uint64_t ppn = alloc_page_frame(); // TODO : need to understand how we continue and mapping only in the end
-                uint64_t* va = phys_to_virt(ppn << 12);
-                pageTable[i][vpnParts[i]] = (va & (~0xFFF)) | 1; // creating the mapping, nullifying the least 12 bits and validating the mapping
+                
+            }
+            if ((pte & 1) == 0) // reached invalid pte, validate it and write the new ppn
+            {
+                newPpn = alloc_page_frame(); // TODO : need to understand how we continue and mapping only in the end
+                currentVa[vpnPart] = (newPpn << 12) | 1; // nullifying the least 12 bits and validating the mapping
                 i--; // making sure the loop continues from the mapping we have just created
             }
             // if pte is valid, continue to the next level
@@ -45,18 +51,19 @@ void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn)
 
 uint64_t page_table_query(uint64_t pt, uint64_t vpn)
 {
-    break_vpn_to_parts(vpnParts, vpn);
     for (int i = 0; i < NUM_OF_LEVELS; i++)
     {
-        uint64_t pte = pageTable[i][vpnParts[i]];
-        if ((pte & 1) == 0) // reached invalid pte, mapping does not exist
+        shift = 36 - (9 * i); 
+        vpnPart = (vpn >> shift) & 0x1FF; // masking to get relevant 9 bits
+        pte = currentVa[vpnPart];
+        if (pte == NULL || (pte & 1) == 0) // reached invalid or not existing pte, no need to invalidate
         {
             return NO_MAPPING;
         }
         // if pte is valid, continue to the next level
+        currentVa = phys_to_virt(pte << 12);
     }
-    uint64_t ppn = pageTable[NUM_OF_LEVELS - 1][vpnParts[NUM_OF_LEVELS - 1]];
-    return ppn; // TODO: Check if that is what we need to return
+    return pte << 12; // TODO: Check if that is what we need to return
 }
 
 void break_vpn_to_parts(uint64_t* vpnPartsAddr, uint64_t vpn)
